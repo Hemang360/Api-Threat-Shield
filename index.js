a64afe499773010855288c847b8e7d98f965b21d
Hemang Bhagat <bhagathemang360@gmail.com>
2025-03-29 15:13:27 +0530

    Fix: ### Issue: Lack of Input Validation

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Export the ApiThreatShield middleware
 module.exports = require('./src/middleware/ApiThreatShield');
 
-const unusedVar = "This variable is never used"; 
-
 function fetchData(url) {
     fetch(url)
         .then(response => response.json())
@@ -11,16 +9,14 @@ function fetchData(url) {
 }
 
 function sum(a, b) {
-    return a + b;
+    return a + Number(b);
 }
 
-const userInput = eval("2 + 2");
+const userInput = 2 + 2;
 
 console.log(sum(10, "5"));
 
 fetchData("https://api.example.com/data");
 
-console.log("Potato");;
-console.log(Shreyas);
-
-random spam Message;
+console.log("Potato");
+console.log("Shreyas");
