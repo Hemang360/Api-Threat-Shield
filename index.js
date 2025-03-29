441304ca0a42b7c77ef357b2e7b7ae91ec90caf9
Hemang Bhagat <bhagathemang360@gmail.com>
2025-03-29 08:48:41 +0530

    Fix: Issue: Syntax and runtime errors due to invalid co...

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 // Export the ApiThreatShield middleware
 module.exports = require('./src/middleware/ApiThreatShield');
-consoleeeeeee.lossssssssssg("Hello");;;;;;;;;;;
-spam Message
-sjsjsjsjsooo
-WebGLTransformFeedbacddd.log(Heao')ooo;
