a3b989ab24e3d4b3259b92224461fb92f5e005fe
Hemang Bhagat <bhagathemang360@gmail.com>
2025-03-29 15:39:00 +0530

    Fix: Given the limited scope of the provided code snipp...

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Export the ApiThreatShield middleware
 module.exports = require('./src/middleware/ApiThreatShield');
 
-const unusedVar = "This variable is never used"; 
-
 function fetchData(url) {
     fetch(url)
         .then(response => response.json())
@@ -16,11 +14,8 @@ function sum(a, b) {
 
 const userInput = eval("2 + 2");
 
-console.log(sum(10, "5"));
+console.log(sum(10, parseInt("5")));
 
 fetchData("https://api.example.com/data");
 
-console.log("Potato");;
-console.log(Shreyas);
-
-random spam Message;
+console.log("Potato");
